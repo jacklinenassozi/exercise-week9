9baa83b46ac89290493b3c504a31108f0f3dd21a
Jack <jack@Jacks-MacBook-Pro.local>
2018-06-03 19:35:11 +0200

    added files

--- main.js
+++ main.js
@@ -1,37 +1,55 @@
-console.log('output')
-function studentsFactory(){
- const addName = (newName)=>{
-     initialStudents.push(newName)
- }
- const changeRoom = (newRoom)=>{
-    room = newRoom
-}
-const deleteName = ()=>{
-    var index = initialStudents.indexOf(oldName);
+console.log('it works')
+function studentsFactory(initialStudents,classRoom){
+  const addName = (newStudentName)=>{
+    initialStudents.push(newStudentName)
+  };
+  const deleteName = (oldStudentName)=>{
+    var index = initialStudents.indexOf(oldStudentName);
     if(index > -1){
       initialStudents.splice(index,1)
     }
-}
-const printAll = ()=>{
-     
-}
+  }
 const deleteAll = ()=>{
-    initialStudents = [];
+  initialStudents=[]
+}
+const changeRoom = (newRoom)=>{
+  classRoom=newRoom
+}
+const printAll = () =>{
+  
+  if(initialStudents.length == 0) {
+    console.log('Zero students');
+    //return;
+} else {
+    const stringOfinitialStudents = [initialStudents.slice(0, -1).join(', '), initialStudents.slice(-1)[0]].join(initialStudents.length < 2 ? '' : ' and ');
+    return stringOfinitialStudents + ' from ' + classRoom;
 }
 
-    const obj = {
-        addName:addName,
-        changeRoom:changeRoom,
-        deleteName:deleteName,
-        changeRoom:changeRoom,
-        deleteAll:deleteAll,
-        printAll:printAll
-    }
-    return obj
 }
+const obj={
+  addName:addName,
+  deleteName:deleteName,
+  deleteAll:deleteAll,
+  changeRoom:changeRoom,
+  printAll:printAll
+}
+return obj
+}
+
 const initialStudents = ["jovia", 'ritah',"madis"];
+console.log(initialStudents.slice(1, 3));//delete from behind
 const room = "1a";
 const students = studentsFactory(initialStudents, room);
 console.log(initialStudents)
-
+students.printAll();
+students.addName('younes');
+console.log("Younes Added ", students.printAll());
+students.addName('john');
+console.log("Johny Added", students.printAll());
+students.deleteName('younes');
+console.log("younes deleted", students.printAll());
+students.changeRoom('1b');
+console.log("classRoom changed", students.printAll());
+students.deleteAll();
+console.log("All students deleted", students.printAll());
 
